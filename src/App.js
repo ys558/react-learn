e7f904d09d1e6c44698002932414403c83d1599a
ys558 <yuyi.gz@163.com>
2019-11-05 08:26:10 +0800

    react基本api、函数和类组件、状态和改变状态

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import { Welcome1, Welcome2 } from './components/01CompType'
+import Clock  from './components/02State-setState'
+import StateTest from './components/03StateTest'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const formatName = (user) => {
+  return `${user.firstName} ${user.lastName}`
+}
+export default class App extends Component {
+  render() {
+    const name = 'jerry'
+    const user = { firstName: `tom`, lastName: `jerry` }
+    const jsx = <p>hello, jerry</p>
+    return (
+      <div>
+        {/* <h3>React Course</h3> */}
+        {/* 1. 基础语法： */}
+        {/* 1.1 表达式 */}
+        {/* <h2>{name}</h2> */}
+        {/* <h2>{formatName(user)}</h2> */}
+        {/* 1.2 属性 */}
+        {/* <img src={logo} style={{ width: '100px' }} /> */}
+        {/* 1.3 jsx也是表达式 */}
+        {/* {jsx} */}
+
+        {/* 2. 使用其他组件 */}
+        {/* <Welcome1 name="some content111"></Welcome1> */}
+        {/* <Welcome2 name="some content2222"></Welcome2> */}
+
+        {/* 3. 组件的状态和设置状态 */}
+        {/* <Clock></Clock> */}
+
+        {/* 4. state的常见问题： */}
+        <StateTest></StateTest>
+      </div>
+    )
+  }
 }
 
-export default App;
---
+++ src/components/03StateTest.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class StateTest extends Component {
+    state = {
+        counter: 1
+    }
+
+    componentDidMount() {
+        // 1. 直接修改状态值无效：只能this.setState({ })更新：
+        // this.state.counter += 1
+
+        // this.setState({ counter: this.state.counter + 1})
+        // 2. setState批量执行时，即改变多次状态时，需用函数式写法：
+        // !!! 除了可以传入对象setState(obj, callback)，也可传入函数setState(fn, callback)
+        this.setState(prevState =>  ({counter: prevState.counter + 1}))
+        this.setState(prevState =>  ({counter: prevState.counter + 1}))
+
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.counter}
+            </div>
+        )
+    }
+}
