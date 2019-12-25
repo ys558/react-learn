66237c42a5a377923834fe4524f52a425c86693c
ys558 <yuyi.gz@163.com>
2019-12-25 14:30:33 +0800

    反向继承增加hijack(劫持)内容

--- src/components/09inheritanceInversion01.js
+++ src/components/09inheritanceInversion01.js
@@ -1,29 +1,44 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 
 // iiHoc return的组件通过继承，拥有了Usual的生命周期及属性，所以didMount会打印，state也通过constructor执行
-// 
+// 基本功能 super.render(),
+// Comp为父组件, 即要强化的那个组件的意思:
 const iiHoc = Comp => class extends Comp {
   render() {
-    console.log(this.state, 'state');
-    const ele = super.render()
+    const parentComponent = super.render()
+    // console.log('xxx', xxx)
+    // xxx {$$typeof: Symbol(react.element), type: "div", key: null, ref: null, props: {…}, …}
     return <div>
-      {ele}
+      {parentComponent}
       <h3>我是子组件内容</h3>
     </div>
+  }
+
+}
+
+// 扩展: hijack(劫持)渲染劫持高阶组件:
+// (config)当做要传入的参数, 特意用括号括起; 
+const hijackRederHOC = (config) => Comp => class extends Comp {
+  render() {
+    const { style = {} } = config;
+    const parentComponent = super.render()
+    if (config.type == 'add-style') {
+      return <div style={{...style}}>
+        {parentComponent}
+      </div>;
+    }
+    return parentComponent
   }
 }
 
 @iiHoc
-export default class InheritanceInversion extends Component {
+@hijackRederHOC({ type: 'add-style', style: {backgroundColor: 'green'} })
+export default class InheritanceInversion extends PureComponent {
   constructor() {
     super();
     this.state = {
       usual: 'usual',
     }
-  }
-
-  componentDidMount() {
-    console.log('didMount')
   }
 
   render() {
