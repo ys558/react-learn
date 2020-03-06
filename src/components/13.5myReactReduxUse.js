f19d875addc58959eac08086176de97469462a69
ys558 <yuyi.gz@163.com>
2020-03-07 07:57:03 +0800

    13.5解析react-redux源码, 为一HOC

---
+++ src/components/13.5myReactReduxUse.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import store from '../store/13.4myStore'
+
+export default class MyReactRedux extends Component {
+  render() {
+    return (
+      <div>
+        <div>{store.getState()}</div>
+        <button onClick={()=> store.dispatch({type: 'PLUS', payload: 2})}>+2</button>
+        <button onClick={()=> store.dispatch({type: 'MINUS'})}>-</button>
+        <button onClick={()=> store.dispatch(()=> setTimeout(()=> store.dispatch({type: 'PLUS'}), 1000))}>async +</button>
+      </div>
+    )
+  }
+}
